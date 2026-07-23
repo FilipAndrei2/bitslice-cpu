07c41ff7b0ba5f499593cfec45186b78a7510732
Filip Andrei-Robert <filandrei4@protonmail.com>
2026-07-23 14:27:46 +0300

    feat: rewrote AM2910 first 8 microinstructions

--- cpu-bitslice.srcs/sources_1/new/am2910.v
+++ cpu-bitslice.srcs/sources_1/new/am2910.v
@@ -1,175 +1,247 @@
 `timescale 1ns / 1ps
 
-// Secventiator
-    module am2910(
-    // Inputs
+// Lungimea unei instructiuni este de 12 biti
+module am2910(
     
-    // Declanseaza toate schimbarile interne pe rising edge
+    // Toate schimbarile interne au loc pe posedge.
     input clk,
     
-    // Direct input, folosit pentru branching
-    input [11:0] direct ,
+    // Input direct pentru registre
+    input [11:0] direct,
     
-    /* Folosit pentru alegerea instructiunii
-        INSTRUCTIUNI IMPLEMENTATE:
-        
-        2 - JUMP MAP 
-            - Instructiune neconditionata care cauzeaza outputul map_not sa fie enabled, astfel incat urmatoarea 
-                locatie de microinstructiune sa fie determinata din adresa oferita din mapping PROM. 
+    // Magistrala de microinstructiuni pentru secventiator
+    input [3:0] instr, 
     
-        3 - CONDITIONAL JUMP PIPELINE
-        - Obtine adresa catre care sa sara din valoarea registrului de pipeline (BR0:BR11)
-        
-        4 - PUSH/CONDITIONAL LOAD COUNTER
-        - Folosit pentru a seta bucle in microprogram.
-        - 
-    */
-    input [3:0] instruction,
-    
-    // Folosit ca si criteriu de test. Trece testul daca CCN este LOW
+    // Folosit pentru salturi bazate pe conditii. O valoare de LOW face ca testul sa treaca.
     input condition_code_not,
     
-    // Daca semnalul este HIGH, CCN este ignorat si secventiatorul opereaza de parca CCN este TRUE (LOW)
-    input condition_code_enable,
-
-    // Carry input pentru incrementer
-    input carry_in, 
-
-    // Daca este LOW, forteaza incarcarea registrelor indiferent de instructiune sau conditie
+    // Daca CCEN este HIGH, CCN este ignorat si testul nu trece.
+    input condition_code_enable_not,
+    
+    input carry_in,
+    
+    // Daca este LOW, forteaza incarcarea registrului R  indiferent de instructiune sau conditie.
     input register_load_not,
     
+    // Daca este LOW, magistrala de iesire Y va lua valoarea high impedance.
     input output_enable_not,
     
-    // Selecteaza input source nr 1 (de obicei registrul de pipeline).
-    input pipeline_address_enable_not,
+    // Adresa catre memoria de microprogram.
+    output reg [11:0] y,
+
+    // O valoare de LOW indica prezenta a 5 elemente pe stiva.
+    output full_not,
     
-    // Selecteaza input source nr 2 (de obicei Mapping PROM sau PLA)
-    input map_address_enable_not,
+    /*
+        Can select #1 source (usually Pipeline Register) as direct
+    input source 
+    */
+    output reg pipeline_address_enable_not,
     
-    // Selecteaza input source nr 3 (de exemplu, Interrupt starting address)
-    input vector_address_enable_not,
+    /*
+        Can select #2 source (usually Mapping PROM or PLA) as
+    direct input source 
+    */
+    output  reg map_address_enable_not,
     
-    // Outputs
+    /*
+        Can select #3 source (for example, Interrupt Starting Address)
+    as direct input source
+    */
+    output reg vector_address_enable_not
+);
+
+// Stiva de microinstructiuni. Capacitate maxima 5.
+reg [11:0] stack [4:0];
+ 
+// Stack pointer. Stiva are capacitate maxima 5.
+reg [2:0] sp; 
+ 
+// Registrul de microprogram, care de obicei contine o adresa cu 1 mai mare decat adresa oferita anterior.
+reg [11:0] uPC;
+
+// Registrul asta contine date incarcate in microinstructiunea anterioara. 
+reg [11:0] R;
+
+// Testul folosit de salturile conditionate.
+wire test;
+assign test = !condition_code_enable_not && !condition_code_not; 
+
+initial begin
+
     
-    // Adresa catre memoria de microprogram
-    output reg [11:0] Y ,
-    
-    // Flag care indica daca stiva este plina. Capacitatea stivei este de 5 elemente.
-    output reg full_not
-    
-  );
-  
-  wire test;
-  assign test = !condition_code_not || condition_code_enable;
-  
-  reg   [15:0] instruction_register = 16'h0;
-  
-  // Tine adresa instructiunii curente
-  reg   [11:0] uPC = 12'b0;
-  
-  reg [11:0] stack [4:0];
-  
-  // Initializare stiva
-  initial begin
-    stack[0] = 0;
-    stack[1] = 0;
-    stack[2] = 0;
-    stack[3] = 0;
-    stack[4] = 0;
-  end
-  
-  reg sp = 0;
-  
-  always @(*) begin
-    case (instruction) 
-    
-        /*
-        0 - Jump Zero (JZ)
-        - Sare la inceputul programului
-        */
-        4'b0000 : begin 
-            uPC = 0;
-            Y = 0;
-        end
+    // Initializare registre
+    uPC     = 12'b000000000000;
+    R       = 12'b000000000000;
+end
+ 
+always@(posedge clk) begin
+    pipeline_address_enable_not <= 1'b1;
+    map_address_enable_not      <= 1'b1;
+    vector_address_enable_not   <= 1'b1;
+    case (instr)
         
-        /*
-        1 - COND JSB PL
-        - Daca testul trece, pune in stiva adresa urmatoarei instructiuni pentru return, dupa care sare la o adresa
-        - Se bazeaza ca inputul CC_NOT sa fie LOW, pentru ca testul sa treaca
-        */
-        4'b0001 : begin 
-            if (test) begin 
-                // pushez in stiva uPC
-                if (sp < 5) begin // daca am capacitate sa pushez in stiva
-                    stack[sp] = uPC + 1;
-                    sp = sp + 1;
-                end else begin // stiva e plina
-                    
-                    // Intrebare: Ce se intampla daca stiva e plina??
-                    // R: Se suprascrie varful stivei, dar sp nu se incrementeaza (Pagina 6-157, paragraful stanga jos)
-                    stack[4] = uPC + 1;
-                end
-                uPC = direct;
-            end else begin
-                uPC = uPC + 1;
-            end
-            Y = uPC;
-        end
         
-        /*
-            2 - JUMP MAP
-            
-        */
-        4'b0011 : begin end
-        4'b0100 : begin end
-        4'b0101 : begin end
-        4'b0110 : begin end
-        4'b0111 : begin end
-        4'b1000 : begin end
-        4'b1001 : begin end
-        4'b1010 : begin end
-        4'b1011 : begin end
-        4'b1100 : begin end
-        4'b1101 : begin end
-        4'b1110 : begin end
-        4'b1111 : begin end
-    endcase
-  end
-  
-  // Calculez flagul full
-  always @(sp) begin
-    full_not = !(sp == 4);
-  end
-  
-  always @(posedge clk) begin
-    case (instruction) 
-    
-        /*
-        0 - Jump Zero (JZ)
-        - Sare la inceputul programului
-        */
-        4'b0000 : begin 
+        // 0: JZ - JUMP ZERO / RESET
+        // Sare la inceputul M
+        4'b0000 : begin
+            // Initializare stiva
+            stack[0] = 12'b000000000000;
+            stack[1] = 12'b000000000000;
+            stack[2] = 12'b000000000000;
+            stack[3] = 12'b000000000000;
+            stack[4] = 12'b000000000000;
+            sp = 3'b000;    
+        
             uPC <= 0;
+            y <= 0;
+            
+            pipeline_address_enable_not <= 1'b0;
         end
         
-        4'b0001 : begin 
+        // 1: CJS : CONDITIONAL JUMP to SUBROUTINE
+        4'b0001 : begin
+            pipeline_address_enable_not <= 1'b0;
+            if (test) begin // sare si pune in stiva adresa de return
+                if (sp < 3'd5) begin
+                    stack[sp] = uPC + 12'd1;
+                    sp = sp + 3'd1;
+                end else begin
+                    // Stiva este plina -> Ultimul element este suprascris
+                    stack[4] = instr;
+                    // nu se incrementeaza sp
+                end
+                y <= direct; // Sare la adresa specificata
+            end else begin // test failed
+                // Continua secvential
+                y <= uPC;
+            end
+            
+
         end
-        4'b0010 : begin end
-        4'b0011 : begin end
-        4'b0100 : begin end
-        4'b0101 : begin end
-        4'b0110 : begin end
-        4'b0111 : begin end
-        4'b1000 : begin end
-        4'b1001 : begin end
-        4'b1010 : begin end
-        4'b1011 : begin end
-        4'b1100 : begin end
-        4'b1101 : begin end
-        4'b1110 : begin end
-        4'b1111 : begin end
+        
+        // 2: JMAP - Jump Map
+        // Sare neconditionat la map si pune in stiva adresa de return
+        4'b0010 : begin
+            map_address_enable_not <= 1'b0;
+            y <= direct; // Sare la adresa specificata de PROM
+        end
+        
+        // 3: CJP - Conditional Jump PL
+        4'b0011 : begin
+            pipeline_address_enable_not <= 1'b0;
+            if (test) begin
+                y <= uPC;
+            end else begin
+                y <= direct;
+            end
+        end
+        
+        // 4: PUSH
+        4'b0100 : begin
+            pipeline_address_enable_not <= 1'b0;
+            
+            // Se baga in stiva next addr
+            if (sp < 3'd5) begin
+                    stack[sp] = uPC + 12'd1;
+                    sp = sp + 3'd1;
+                end else begin
+                    // Stiva este plina -> Ultimul element este suprascris
+                    stack[4] = instr;
+                    // nu se incrementeaza sp
+             end
+             
+             // Trebuie incarcat conditionat R
+             if (test) begin
+                R <= direct;
+             end
+             
+             y <= uPC;
+        end
+        
+        // 5: JSRP - COND JSB R/PL
+        4'b0101 : begin
+            pipeline_address_enable_not <= 1'b0;
+            // Se baga in stiva next addr
+            if (sp < 3'd5) begin
+                    stack[sp] = uPC + 12'd1;
+                    sp = sp + 3'd1;
+                end else begin
+                    // Stiva este plina -> Ultimul element este suprascris
+                    stack[4] = instr;
+                    // nu se incrementeaza sp
+            end
+            
+            if (test) begin
+                y <= direct;
+            end else begin
+                y <= R;
+            end
+        end
+        
+        // 6 : CJV - COND JUMP VECTOR
+        4'b0110 : begin
+            vector_address_enable_not <= 1'b0;
+            
+            if (test) begin
+                y <= direct;
+            end else begin
+                y <= uPC;
+            end
+        end
+        
+        // 7 : JRP - COND JUMP R/PL
+        4'b0111 : begin
+            pipeline_address_enable_not <= 1'b0;
+            
+            if (test) begin
+                y <= direct;
+            end else begin
+                y <= R;
+            end
+        end
+        
+        // 8 : RFCT 
+        // TODO: BAGA MARE DE AICI
+        4'b1000 : begin
+            if (R == 12'd0) begin
+            
+            end else begin
+                
+            end
+        end
+        
+        4'b1001 : begin
+        
+        end
+        
+        4'b1010 : begin
+        
+        end
+        
+        4'b1011 : begin
+        
+        end
+        
+        4'b1100 : begin
+        
+        end
+        
+        4'b1101 : begin
+        
+        end
+        
+        4'b1110 : begin
+        
+        end
+        
+        4'b1111 : begin
+        
+        end
+        
     endcase
-  end
-  
+    
+    uPC <= y + carry_in;
+end
   
 endmodule
